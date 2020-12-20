084038de1fcf3f61fdf4082a4c7aaa10ec9faa80
Wahid Morian <wmorian@pm.me>
2020-12-20 22:09:12 +0100

    add 'new' page for creating new campaigns

---
+++ pages/campaigns/new.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { Form, Button, Input, Message } from 'semantic-ui-react';
+import Layout from '../../components/Layout';
+import factory from '../../ethereum/factory';
+import web3 from '../../ethereum/web3';
+
+const CampaignNew = () => {
+
+    const [minimumContribution, setMinimumContribution] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const createNewCampaign = async (event) => {
+        event.preventDefault();
+
+        setLoading(true);
+        setErrorMessage("");
+        try {
+            const accounts = await web3.eth.getAccounts();
+            await factory.methods.createCampaign(minimumContribution)
+                .send({ from: accounts[0] });   
+        } catch (error) {
+            setErrorMessage(error.message);
+        }
+        setLoading(false);
+    };
+
+    return (
+        <Layout>
+            <h3>Create a new Campaign</h3>
+            <Form onSubmit={createNewCampaign} error={!!errorMessage}>
+                <Form.Field>
+                    <label>Minimum Contribution</label>
+                    <Input 
+                        label='wei' 
+                        labelPosition='right' 
+                        value={minimumContribution}
+                        onChange={e => setMinimumContribution(e.target.value)}
+                    />
+                </Form.Field>
+
+                <Message error header="Oops!" content={errorMessage} />
+                <Button primary loading={loading}>Create!</Button>
+            </Form>
+        </Layout>
+    )
+};
+
+export default CampaignNew;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import 'semantic-ui-css/semantic.min.css';
-import { CardGroup, Button, Menu } from 'semantic-ui-react';
+import { CardGroup, Button } from 'semantic-ui-react';
 import Layout from '../components/Layout';
 import factory from '../ethereum/factory';
 
